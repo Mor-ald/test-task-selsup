8ef659fbcd0b3d73f056559b5fc2a76186d0e5e1
Mor-ald <mor.ald@yandex.ru>
2025-01-30 16:23:02 +0300

    add initial data and ParamEditor component with h1

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,35 @@
-const App = () => {
-  return <></>;
+import ParamEditor, { Model, Param } from './components/param-editor/ParamEditor';
+
+import React, { ReactNode } from 'react';
+
+const params: Param[] = [
+  { id: 1, name: 'Назначение', type: 'string' },
+  { id: 2, name: 'Длина', type: 'string' },
+];
+
+const model: Model = {
+  paramValues: [
+    {
+      paramId: 1,
+      value: 'повседневное',
+    },
+    {
+      paramId: 2,
+      value: 'макси',
+    },
+  ],
+  colors: [],
 };
+
+class App extends React.Component {
+  render(): ReactNode {
+    return (
+      <>
+        <h1>Редактор параметров</h1>
+        <ParamEditor params={params} model={model} />
+      </>
+    );
+  }
+}
 
 export default App;
